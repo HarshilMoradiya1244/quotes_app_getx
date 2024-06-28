986f49d947c02f6e5943593bcd51e81bb37710c2
vivek <Katariyavivek228@gmail.com>
2024-06-28 14:18:35 +0530

    new

--- lib/screen/quotes/view/quotes_screen.dart
+++ lib/screen/quotes/view/quotes_screen.dart
@@ -39,83 +39,72 @@ class _QuotesScreenState extends State<QuotesScreen> {
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.all(10.0),
-              child: InkWell(
-                onTap: () {
-                  Get.toNamed("detail", arguments: [
-                    model.bgImageList[index],
-                    model.category,
-                    model.qoutesList[index],
-                    model.authorList[index],
-                    model.id
-                  ]);
-                },
-                child: Container(
-                  height: MediaQuery.sizeOf(context).height * 0.18,
-                  width: MediaQuery.sizeOf(context).width,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: colors[index],
-                  ),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        "${model.bgImageList[index]}",
-                        height: MediaQuery.sizeOf(context).height,
-                        width: MediaQuery.sizeOf(context).width,
-                        fit: BoxFit.cover,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              width: 300,
-                              child: Text(
-                                "${model.qoutesList[index]}",
-                                style: const TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                                overflow: TextOverflow.clip,
-                                maxLines: 2,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "${model.authorList[index]}",
+              child: Container(
+                height: MediaQuery.sizeOf(context).height * 0.18,
+                width: MediaQuery.sizeOf(context).width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: colors[index],
+                ),
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      "${model.bgImageList[index]}",
+                      height: MediaQuery.sizeOf(context).height,
+                      width: MediaQuery.sizeOf(context).width,
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 300,
+                            child: Text(
+                              "${model.qoutesList[index]}",
                               style: const TextStyle(
                                   fontSize: 15,
                                   fontWeight: FontWeight.bold,
                                   color: Colors.black),
+                              overflow: TextOverflow.clip,
+                              maxLines: 2,
                             ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: IconButton(
-                                onPressed: () {
-                                  DbModel dbModel = DbModel(
-                                      author: model.authorList[index],
-                                      quotes: model.qoutesList[index],
-                                      category: model.category);
-                                  DBHelper dbHelper = DBHelper();
-                                  dbHelper.insertData(dbModel);
-                                  Get.snackbar(
-                                      "Add Successfully", "Check In Favorite");
-                                },
-                                icon: const Icon(
-                                  Icons.favorite_border,
-                                  color: Colors.black,
-                                ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "${model.authorList[index]}",
+                            style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: IconButton(
+                              onPressed: () {
+                                DbModel dbModel = DbModel(
+                                    author: model.authorList[index],
+                                    quotes: model.qoutesList[index],
+                                    category: model.category);
+                                DBHelper dbHelper = DBHelper();
+                                dbHelper.insertData(dbModel);
+                                Get.snackbar(
+                                    "Add Successfully", "Check In Favorite");
+                              },
+                              icon: const Icon(
+                                Icons.favorite_border,
+                                color: Colors.black,
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             );
